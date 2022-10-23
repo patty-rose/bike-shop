4e08caa0b09ee6cdcc3928c26d556a842144362b
Patty Otero <101524326+patty-rose@users.noreply.github.com>
2022-10-22 23:10:31 -0700

    add card styling to bike list items

--- src/components/Bike.js
+++ src/components/Bike.js
@@ -1,17 +1,35 @@
 import React from "react";
 import PropTypes from"prop-types";
+import bike from "./../img/bike.png";
 
 function Bike(props){ 
   return(
     <React.Fragment>
-      <div onClick = {() => props.whenBikeClicked(props.id)}>
-        <h3>{ props.size } -{ props.type } </h3>
-        <p>{ props.count}</p>
+      <div class="pt-6 pb-12">  
+        <div id="card" class="">
+          <div class="container w-100 lg:w-4/5 mx-auto flex flex-col">
+
+            <div v-for="card in cards" class="flex flex-col md:flex-row overflow-hidden bg-white rounded-lg shadow-xl  mt-4 w-100 mx-2">
+
+              <div class="h-64 w-auto md:w-1/2" style={{cursor:'pointer'}} onClick = {() => props.whenBikeClicked(props.id)}>
+                <img class="inset-0 h-full w-full object-cover object-center" src={bike} alt="black bike on a wooden floor against a white wall" />
+              </div>
+
+              <div class="w-full py-4 px-6 text-gray-800 flex flex-col justify-between">
+                <h3 class="font-semibold text-lg leading-tight truncate">Size: { props.size }</h3>
+                <h3 class="font-semibold text-lg leading-tight truncate">Type: { props.type }</h3>
+                <h2 class="font-semibold text-lg leading-tight truncate">In stock: { props.count }</h2>
+                <p class="mt-2">
+                  <button className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type='button'onClick = {() => props.whenSellBikeClicked(props.id)}>Sell one!</button>
+                  <button className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type='button'onClick = {() => props.whenScrapBikeClicked(props.id)}>Scrap One</button>
+                  <button className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type='button'onClick = {() => props.whenGrantBikeClicked(props.id)}>Grant one!</button>
+                  <button className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type='button'onClick = {() => props.whenIncrementBikeCountClicked(props.id)}>Add one!</button>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <button onClick = {() => props.whenSellBikeClicked(props.id)}>Sell one!</button>
-      <button onClick = {() => props.whenScrapBikeClicked(props.id)}>Scrap One!</button>
-      <button onClick = {() => props.whenGrantBikeClicked(props.id)}>Grant one!</button>
-      <button onClick = {() => props.whenIncrementBikeCountClicked(props.id)}>+1</button>
     </React.Fragment>
   )
 }
--- src/components/ReusableBikeForm.js
+++ src/components/ReusableBikeForm.js
@@ -5,28 +5,28 @@ function ReusableBikeForm(props) {
   return (
     <React.Fragment>
       <form onSubmit={props.formSubmissionHandler}>
-        <div class="mb-3 pt-0">
+        <div className="mb-3 pt-0">
           <input
             type='text'
             name='size'
             placeholder='Bike Size'
-            class="px-3 py-4 placeholder-slate-500 text-slate-900 relative bg-white bg-white rounded text-base border-0 shadow outline-none focus:outline-none focus:ring w-full" />
+            className="px-3 py-4 placeholder-slate-500 text-slate-900 relative bg-white bg-white rounded text-base border-0 shadow outline-none focus:outline-none focus:ring w-full" />
         </div>
-        <div class="mb-3 pt-0">
+        <div className="mb-3 pt-0">
           <input
             type='text'
             name='type'
             placeholder='Bike Type'
-            class="px-3 py-4 placeholder-slate-500 text-slate-900 relative bg-white bg-white rounded text-base border-0 shadow outline-none focus:outline-none focus:ring w-full"  />
+            className="px-3 py-4 placeholder-slate-500 text-slate-900 relative bg-white bg-white rounded text-base border-0 shadow outline-none focus:outline-none focus:ring w-full"  />
         </div>
-        <div class="mb-3 pt-0">
+        <div className="mb-3 pt-0">
           <input
             type='number'
             name='count'
             placeholder='Quantity of bikes being added'
-            class="px-3 py-4 placeholder-slate-500 text-slate-900 relative bg-white bg-white rounded text-base border-0 shadow outline-none focus:outline-none focus:ring w-full" />
+            className="px-3 py-4 placeholder-slate-500 text-slate-900 relative bg-white bg-white rounded text-base border-0 shadow outline-none focus:outline-none focus:ring w-full" />
         </div>
-        <button class="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-base px-8 py-3 rounded shadow-md hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type='submit'>{props.buttonText}</button>
+        <button className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-base px-8 py-3 rounded shadow-md hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type='submit'>{props.buttonText}</button>
       </form>
     </React.Fragment>
   );
