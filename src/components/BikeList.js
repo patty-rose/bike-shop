b785177641e3a5e74108c672a7d5074f6a1b9691
Patty Otero <101524326+patty-rose@users.noreply.github.com>
2022-10-23 10:58:38 -0700

    update error handling on bikeList

--- src/components/BikeList.js
+++ src/components/BikeList.js
@@ -3,9 +3,18 @@ import Bike from "./Bike";
 import PropTypes from "prop-types";
 
 function BikeList(props){
+const list = props.bikeList;
+let noBikes = null;
+if (list.length === 0){
+  noBikes = <div class="bg-green-100 border-t border-b border-green-500 text-green-700 px-4 py-3 text-center" role="alert">
+  <p class="font-bold">No Bikes In Stock!!! </p>
+  <p class="text-sm">Click Add Bikes in the navigation bar above to get started!</p>
+</div>
+}
+
   return (
     <React.Fragment>
-        <h1 className="font-bold leading-relaxed uppercase text-center">Inventory</h1>
+      <h1 className="font-bold leading-relaxed uppercase text-center">Inventory</h1>
       {props.bikeList.map((bike) =>
         <Bike
           whenIncrementBikeCountClicked = { props.onIncrementBikeCountClicked }
@@ -19,6 +28,7 @@ function BikeList(props){
           id={bike.id}
           key={bike.id}/>
       )}
+      {noBikes}
     </React.Fragment>
   );
 }
